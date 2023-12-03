a50e5e02aaabbf0278c4cc4daea0db57f1c04953
MasterCoookie <jas.kocurek2@wp.pl>
2023-12-03 13:39:14 +0100

    player class

--- script.py
+++ script.py
@@ -25,16 +25,34 @@ screen.clear(psp2d.Color(0,0,0,255))
 
 # Loads the font
 font = psp2d.Font('font.png')
-ball=psp2d.Image('ball.png')
-ball_size=9,9
-ball_position=[width/2,height/2]
-ball_velocity=[0,0]
+# ball=psp2d.Image('ball.png')
+# ball_size=9,9
+# ball_position=[width/2,height/2]
+# ball_velocity=[0,0]
+
+class Player(psp2d.Image):
+    def __init__(self, *args):
+        psp2d.Image.__init__(self, *args)
+        self.position = [width/2,height/2]
+        # self.size = [9, 9]
+
+    def move(self, velocity):
+        self.position[0] += velocity[0]
+        self.position[1] += velocity[1]
+
+    def draw(self, screen):
+        screen.blit(self, 0, 0, self.size[0], self.size[1], self.position[0], self.position[1], True)
+
+
+player = Player('ball.png')
+
+all_entities = [player]
 
 game_started = True
 while game_started:
     pad = psp2d.Controller()
     velocity = [0,0]
-    if pad.cross:
+    if pad.cross: #z na klawie
         game_started = False
     if pad.left:
         velocity[0] = -4
@@ -45,11 +63,14 @@ while game_started:
     if pad.down:
         velocity[1] = 4
 
-    ball_position[0] += velocity[0]
-    ball_position[1] += velocity[1]
+    # ball_position[0] += velocity[0]
+    # ball_position[1] += velocity[1]
 
     screen.clear(psp2d.Color(0,0,0,255))
-    screen.blit(ball, 0, 0, ball_size[0], ball_size[1], ball_position[0], ball_position[1], True)
+    # screen.blit(ball, 0, 0, ball_size[0], ball_size[1], ball_position[0], ball_position[1], True)
+    for entity in all_entities:
+        entity.move(velocity)
+        entity.draw(screen)
     screen.swap()
 
 
